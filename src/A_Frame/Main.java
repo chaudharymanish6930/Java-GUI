ff756143df3ed0bfd95aaec63e721fe81d8eb452
Manish Choudhary <me.developer4u@gmail.com>
2025-08-29 15:37:41 +0530

    Update

---
+++ src/A_Frame/Main.java
@@ -0,0 +1,18 @@
+package A_Frame;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(); // create a frame
+        frame.setTitle("my first Gui");  // sets title of frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // exit out of the application
+        frame.setResizable(false);  // prevent frame from being resized
+        frame.setSize(450,450);  // set the x-direction and y-direction of frame
+        frame.setVisible(true);  // make frame visible
+
+        ImageIcon image = new ImageIcon("india_png.jpg"); // create an image icon
+        frame.setIconImage(image.getImage()); // change icon of frame
+    }
+}
