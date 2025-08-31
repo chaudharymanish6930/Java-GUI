843400dfc8575d20215dba08564d19ba18c77a90
Manish Choudhary <me.developer4u@gmail.com>
2025-08-31 15:04:52 +0530

    Update

--- src/A_Frame/Main.java
+++ src/A_Frame/Main.java
@@ -2,9 +2,13 @@ package A_Frame;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class Main {
     public static void main(String[] args) {
+
+        // JFrame = a GUI window to add components
+
         JFrame frame = new JFrame(); // create a frame
         frame.setTitle("my first Gui");  // sets title of frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // exit out of the application
@@ -14,5 +18,9 @@ public class Main {
 
         ImageIcon image = new ImageIcon("C:\\Users\\mecha\\IdeaProjects\\Java GUI\\src\\A_Frame\\Image\\india_png.jpg"); // create an image icon
         frame.setIconImage(image.getImage()); // change icon of frame
+
+        frame.getContentPane().setBackground(Color.green);
+        frame.getContentPane().setBackground(new Color(123,50,250));
+
     }
 }
