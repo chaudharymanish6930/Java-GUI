527472195e366d868206141c5c84abb41330204c
Manish Choudhary <me.developer4u@gmail.com>
2025-09-01 21:49:34 +0530

    Update

---
+++ src/C_Panels/a_BasicPanels.java
@@ -0,0 +1,13 @@
+package C_Panels;
+
+import javax.swing.*;
+
+public class a_BasicPanels {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(550,550);
+        frame.setLayout(null);   // main page is empty for the method
+        frame.setVisible(true);
+    }
+}
