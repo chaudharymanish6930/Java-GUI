3c897412764b65a1956b536e4cf4d0ec6c1a377e
Manish Choudhary <me.developer4u@gmail.com>
2025-09-01 22:02:07 +0530

    Update

--- src/C_Panels/a_BasicPanels.java
+++ src/C_Panels/a_BasicPanels.java
@@ -11,11 +11,22 @@ public class a_BasicPanels {
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0,0,250,250);
 
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.CYAN);
+        greenPanel.setBounds(100,100,250,250);
+
+        JPanel yellowPanel = new JPanel();
+        yellowPanel.setBackground(Color.yellow);
+        yellowPanel.setBounds(250,0,250,250);
+
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);   // main page is empty for the method
         frame.setSize(750,750);
         frame.setVisible(true);
         frame.add(redPanel);
+        frame.add(greenPanel);
+        frame.add(yellowPanel);
     }
 }
