da81a53e5ec6fbdc29f94582b7653cebc9401aa8
Manish Choudhary <me.developer4u@gmail.com>
2025-09-01 21:56:02 +0530

    Update

--- src/C_Panels/a_BasicPanels.java
+++ src/C_Panels/a_BasicPanels.java
@@ -1,13 +1,21 @@
 package C_Panels;
 
 import javax.swing.*;
+import java.awt.Color;
 
 public class a_BasicPanels {
     public static void main(String[] args) {
+        // JPanel = a GUI component that functions as a container to hold other components
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,250,250);
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(550,550);
         frame.setLayout(null);   // main page is empty for the method
+        frame.setSize(750,750);
         frame.setVisible(true);
+        frame.add(redPanel);
     }
 }
